eae154d818eae7ce3dfe75553bf7988e3e2f3c72
Praveen <praveenme117@gmail.com>
2024-08-31 23:18:26 +0530

    updated homepage styling

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,11 +1,9 @@
-import Home from '@/modules/home/homepage';
+import Home from "@/modules/home/homepage";
 
 const AppHomePage = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <div className='rounded-xl shadow-sm p-12 font-bold text-5xl text-center'>
-        <Home />
-      </div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Home />
     </div>
   );
 };
--- src/modules/home/homepage.tsx
+++ src/modules/home/homepage.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 
+const images = [
+  { src: "/assets/images/home/person-1.png", alt: "Person 1" },
+  { src: "/assets/images/home/person-2.png", alt: "Person 2" },
+  { src: "/assets/images/home/person-3.png", alt: "Person 3" },
+];
+
 const Home = () => {
   return (
     <section className="h-screen flex flex-col items-center justify-center">
@@ -13,17 +19,37 @@ const Home = () => {
           height={36}
           className="w-6 h-6 object-contain"
         />
-
-        <h1 className="font-black">{APP_NAME}</h1>
+        <h1 className="font-semibold text-xl md:text-3xl">{APP_NAME}</h1>
       </div>
 
-      <h2 className="font-bold text-center text-xl md:text-2xl mb-2">
-        Welcome back to {APP_NAME}
-      </h2>
+      <div className="mx-auto md:p-8 md-6 flex flex-col max-w-3xl items-center gap-1 mb-5 rounded-3xl bg-white">
+        <h2 className="font-semibold text-center text-2xl md:text-4xl mb-2">
+          Welcome to headshot pro
+        </h2>
 
-      <p className="text-gray-500 text-center mb-6">
-        The best way to enhance your Photos with AI
-      </p>
+        <p className="text-gray-500 text-center text-sm mb-6">
+          In publishing and graphic design, Lorem ipsum is a placeholder text
+          commonly used to demonstrate the visual form of a document or a
+          typeface without relying on meaningful content.
+        </p>
+
+        <div className="flex items-center justify-center gap-3 md:gap-4">
+          {images.map((image, index) => (
+            <Image
+              key={index}
+              src={image.src}
+              alt={image.alt}
+              width={800}
+              height={800}
+              className="object-cover w-full h-80 rounded-xl"
+            />
+          ))}
+        </div>
+
+        <button className="mt-6 w-full bg-[#0BAB7C] items-center text-sm justify-center rounded-3xl py-2  text-white/80">
+          Get Started
+        </button>
+      </div>
     </section>
   );
 };
