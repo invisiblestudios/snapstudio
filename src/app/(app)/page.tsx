299c38d88df5b655798dc19aaa0501f503326d53
Praveen <praveenme117@gmail.com>
2024-09-12 23:22:48 +0530

    Add navbar

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/navbar/navbar";
 import Home from "@/modules/home/homepage";
 
 const AppHomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+       <Navbar />
       <Home />
     </div>
   );
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,32 +1,27 @@
 import React from "react";
 
-const Home = () => {
+const Navbar = () => {
   return (
-    <section className="h-screen flex flex-col items-center justify-center">
-      <div className="mx-auto md:p-8 md-6 flex flex-col max-w-3xl items-center gap-1 mb-5 rounded-3xl bg-white">
-        <p className="font-semibold text-center text-2xl md:text-4xl mb-2">
-          <span className="text-primary">1</span>
-          Welcome
+    <nav className="h-screen flex flex-col items-center justify-center">
+      <div className="mx-auto p-6 md:p-8 flex max-w-3xl text-sm items-center gap-4 mb-5 rounded-3xl bg-white shadow-md">
+        <p className="font-semibold text-center text-sm">
+          <span className="text-primary">1</span> Welcome
         </p>
-        <p className="font-semibold text-center text-2xl md:text-4xl mb-2">
-          <span className="text-primary">2</span>
-          Package
+        <p className="font-semibold text-center text-sm">
+          <span className="text-primary">2</span> Package
         </p>
-        <p className="font-semibold text-center text-2xl md:text-4xl mb-2">
-          <span className="text-primary">3</span>
-          Styles
+        <p className="font-semibold text-center text-sm">
+          <span className="text-primary">3</span> Styles
         </p>
-        <p className="font-semibold text-center text-2xl md:text-4xl mb-2">
-          <span className="text-primary">4</span>
-          Upload
+        <p className="font-semibold text-center text-sm">
+          <span className="text-primary">4</span> Upload
         </p>
-        <p className="font-semibold text-center text-2xl md:text-4xl mb-2">
-          <span className="text-primary">5</span>
-          Summary
+        <p className="font-semibold text-center text-sm">
+          <span className="text-primary">5</span> Summary
         </p>
       </div>
-    </section>
+    </nav>
   );
 };
 
-export default Home;
+export default Navbar;
