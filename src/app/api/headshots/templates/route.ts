d424504fc900ee938a2ad04ef8dc208dd88ca09a
ruthvik <ruthv1k@proton.me>
2024-10-01 22:09:37 +0530

    Minor updates to templates handler route

--- src/app/api/headshots/templates/route.ts
+++ src/app/api/headshots/templates/route.ts
@@ -2,14 +2,18 @@ import { routeHandler } from '@/lib/routeHandler';
 import createSupabaseServerClient from '@/lib/supabase/server';
 import { NextResponse } from 'next/server';
 
-const getTemplatesHander = async (request: Request) => {
+const getTemplatesHander = async () => {
   const supabase = await createSupabaseServerClient();
 
   const templates = supabase
     .from('headshots')
     .select('id,name,description,image_preview_url');
 
-  return NextResponse.json({ success: true, message: '', data: templates });
+  return NextResponse.json({
+    success: true,
+    message: 'Fetched templates',
+    data: templates,
+  });
 };
 
 export const GET = routeHandler(getTemplatesHander);
